ac1562804fb04430f9b63492e8476a3de9a41f9f
Tatsuya <49563669+kmno4hno3@users.noreply.github.com>
2025-03-09 19:56:12 +0900

    crate追加準備

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,13 +1,23 @@
 use axum::{routing::get, Router};
+use tower_http::trace::TraceLayer;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
 async fn main() {
-    async fn root_handler() -> String {
-        "Hello World".to_string()
-    }
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
+                format!("{}=debug,tower_http=debug,", env!("CARGO_CRATE_NAME")).into()
+            }),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
 
-    let app = Router::new().route("/", get(root_handler));
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, Axum!!!" }))
+        .layer(TraceLayer::new_for_http());
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    tracing::debug!("listening on {}", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
 }
