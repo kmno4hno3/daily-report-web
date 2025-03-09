ef6ba2b749307d88a8bdf5e84db8563f2c94db90
Tatsuya <49563669+kmno4hno3@users.noreply.github.com>
2025-03-10 08:59:51 +0900

    リポジトリー追加

---
+++ backend/src/domain/repositories/report_repository.rs
@@ -0,0 +1,11 @@
+use crate::domain::models::report::Report;
+use async_trait::async_trait;
+
+#[async_trait]
+pub trait ReportRepository {
+    async fn find_all(&self) -> Result<Vec<Report>, sqlx::Error>;
+    async fn find_by_id(&self, id: i64) -> Result<Option<Report>, sqlx::Error>;
+    async fn create(&self, report: Report) -> Result<Report, sqlx::Error>;
+    async fn update(&self, report: Report) -> Result<Report, sqlx::Error>;
+    async fn delete(&self, id: i64) -> Result<Report, sqlx::Error>;
+}
