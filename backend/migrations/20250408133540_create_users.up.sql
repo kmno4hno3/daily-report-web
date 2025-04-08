b4125598f5b2873debb3d982833a40ca83fa8cb4
Tatsuya <49563669+kmno4hno3@users.noreply.github.com>
2025-04-08 22:42:16 +0900

    usersテーブル追加

---
+++ backend/migrations/20250408133540_create_users.up.sql
@@ -0,0 +1,10 @@
+-- Add up migration script here
+-- Add migration script here
+CREATE TABLE users (
+  id BIGSERIAL PRIMARY KEY,
+  name VARCHAR(100),
+  email VARCHAR(255) NOT NULL UNIQUE,
+  password VARCHAR(255) NOT NULL UNIQUE,
+  created_at TIMESTAMP WITH TIME ZONE DEFAULT (now() AT TIME ZONE 'Asia/Tokyo') NOT NULL,
+  updated_at TIMESTAMP WITH TIME ZONE DEFAULT (now() AT TIME ZONE 'Asia/Tokyo') NOT NULL
+);
