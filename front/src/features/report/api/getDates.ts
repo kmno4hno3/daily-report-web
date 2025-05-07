038584985587a9d33718018300a7753589a7ae14
Tatsuya <49563669+kmno4hno3@users.noreply.github.com>
2025-05-08 00:04:01 +0900

    日付一覧取得にheader追加

--- front/src/features/report/api/getDates.ts
+++ front/src/features/report/api/getDates.ts
@@ -3,15 +3,22 @@
 import { auth } from "@/auth"
 import type { Year } from "@/src/entities/report/type"
 import axios from "axios"
+import { createSignedJwt } from "../model/createJwt"
 
 export const getDates = async (
 	currentYear: number,
 ): Promise<Year | undefined> => {
 	const session = await auth()
 	if (!session?.user) return
+	const token = session?.user ? await createSignedJwt(session.user) : ""
+
 	try {
 		const url = `${process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000"}/api/report/dates/${currentYear}`
-		return await axios.get(url).then((res) => res.data)
+		return await axios
+			.get(url, {
+				headers: { Authorization: `Bearer ${token}` },
+			})
+			.then((res) => res.data)
 	} catch (error) {
 		console.error(error)
 	}
