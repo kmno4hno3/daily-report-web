1b8c767454a202f4bdf8b49f9225dc074a8c1cc0
Tatsuya <49563669+kmno4hno3@users.noreply.github.com>
2025-07-30 15:38:06 +0900

    検索入力debounce対応

--- front/src/features/search/ui/Search.tsx
+++ front/src/features/search/ui/Search.tsx
@@ -4,16 +4,20 @@ import { useFetchReportDates } from "@/src/features/search/hooks/useFetchReportD
 import { Input } from "@/src/shared/ui/input"
 import { Search as SearchIcon } from "lucide-react"
 import { useState } from "react"
+import { useDebouncedCallback } from "use-debounce"
 
 export const Search = () => {
 	const [searchQuery, setSearchQuery] = useState<string>("")
 	useFetchReportDates(searchQuery)
+	const handleSearch = useDebouncedCallback((query: string) => {
+		setSearchQuery(query)
+	}, 300)
 
 	return (
 		<Input
 			icon={<SearchIcon className="text-[#737373]" />}
 			onChange={(e) => {
-				setSearchQuery(e.target.value)
+				handleSearch(e.target.value)
 			}}
 		/>
 	)
